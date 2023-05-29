15c9e105df0d0ef286f543a50f441eeff50dc5d5
馬場義弘 <babayoshihiro@YB-MBP14.local>
2023-05-29 22:29:17 +0900

    coding the front-page

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
-import '../scss/main.scss';
+import '../scss/style.scss';
 
+$(function(){
+    //アコーディオン
+    $(".p-faq__content > dt").off('click');
+    $(".p-faq__content > dt").on('click', function() {
+        $(this).next().slideToggle(400);
+        $(this).find(".c-close").toggleClass("c-open");
+    });
+});
