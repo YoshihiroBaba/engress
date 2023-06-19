75779f17cd1a6d53dbcaeb7ccd311b1e5b0f8d82
馬場義弘 <babayoshihiro@YB-MBP14.local>
2023-06-19 22:48:45 +0900

    front page creat

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 import '../scss/style.scss';
 
 $(function(){
+     // バーガーメニュー
+     $(".c-burger-icon").off('click');
+     $(".c-burger-icon").on('click', function () {
+       $("#c-burger-icon").toggleClass("c-burger-open");
+       $(".l-header__burger").toggleClass("u-dis--show");
+       $(window).scroll(function(){
+         $("#c-burger-icon").removeClass("c-burger-open");
+         $(".l-header__burger").removeClass("u-dis--show");
+       });
+
+     });
     //アコーディオン
     $(".p-faq__content > dt").off('click');
     $(".p-faq__content > dt").on('click', function() {
